4673eaf43a4b7efd5e97fe890a9f9523981524cc
VenkateshSrinivasan <venkateshsrini3@gmail.com>
2018-07-31 08:22:54 +0530

    HTTP certificate handling in docker
    Http certifcate handling in both production and dev mode

---
+++ SampleDockerMVC/Deploy.cmd
@@ -0,0 +1,7 @@
+﻿REM 
+REM dotnet dev-certs https -ep %APPDATA%\ASP.NET\https\SampleDockerMVC.pfx -p crypticpassword
+REM dotnet dev-certs https --trust
+REM dotnet user-secrets -p SampleDockerMVC\SampleDockerMVC.csproj set "Kestrel:Certificates:Development:Password" "crypticpassword"
+SETX APPDATA %AppData%
+REM docker-compose -f docker-compose.yml -f docker-compose.override.Dev.yml up --build --force-recreate
+docker-compose -f docker-compose.yml -f docker-compose.override.prod.yml up --build --force-recreate
